c0418646ff5ecc3256365180c2631e47a3252780
vilyever <250450035@163.com>
2016-08-05 16:11:53 +0800

    first release

---
+++ README.md
@@ -0,0 +1,17 @@
+# objcNotificationCenter
+objcNotificationCenter
+
+## Import
+### [CocoaPods](http://cocoapods.org)
+use_frameworks!
+</br>
+pod 'objcNotificationCenter'
+
+### [Carthage](https://github.com/Carthage/Carthage)
+github "vilyever/objcNotificationCenter" "master"
+
+## Usage
+
+## License
+
+[MIT license](LICENSE)
--- objcNotificationCenter/objcNotificationCenter.h
+++ objcNotificationCenter/objcNotificationCenter.h
@@ -16,4 +16,11 @@ FOUNDATION_EXPORT const unsigned char objcNotificationCenterVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <objcNotificationCenter/PublicHeader.h>
 
+#import <objcNotificationCenter/VDNotificationElement.h>
+#import <objcNotificationCenter/NSObject+VDNotification.h>
 
+
+#if !VDNotificationDefaultCenter
+#define VDNotificationDefaultCenter \
+[NSNotificationCenter defaultCenter]
+#endif
